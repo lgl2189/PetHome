6d9fcabc5efe5aa16db28ec14bec45029655487c
lgl2189 <1214525891@qq.com>
2025-04-30 16:16:14 +0800

    为Result类添加序列化

--- src/main/java/com/pethome/entity/web/Result.java
+++ src/main/java/com/pethome/entity/web/Result.java
@@ -1,8 +1,12 @@
 package com.pethome.entity.web;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.io.Serial;
+import java.io.Serializable;
 
 /**
  * @author ：李冠良
@@ -13,7 +17,11 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Result {
+public class Result implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
     private Object data;
     private String status;
     private String message;
