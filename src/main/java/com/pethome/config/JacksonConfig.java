1e257bf2921f2bb1ecd3788d227103862bddfac2
lgl2189 <1214525891@qq.com>
2025-04-30 16:27:29 +0800

    添加Jackson配置类，添加Jackson对时间的支持

---
+++ src/main/java/com/pethome/config/JacksonConfig.java
@@ -0,0 +1,22 @@
+package com.pethome.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author ：李冠良
+ * @description ：无描述
+ * @date ：2025 4月 30 16:17
+ */
+
+@Configuration
+public class JacksonConfig {
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        return objectMapper;
+    }
+}
