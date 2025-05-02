a7ccd2ba434d9b60c7d7b9940ccc453cc9f8b49e
lgl2189 <1214525891@qq.com>
2025-05-02 15:12:05 +0800

    调整常量类位置

---
+++ src/main/java/com/pethome/constant/Constant.java
@@ -0,0 +1,12 @@
+package com.pethome.constant;
+
+/**
+ * @author ：李冠良
+ * @description ：无描述
+ * @date ：2025 4月 30 15:45
+ */
+
+
+public class Constant {
+    public static final String JWT_SECRET = "OzWsf2X24eKLZbvazvBMXqcrHd0RRhsCOibf+8KfQRc=";
+}
