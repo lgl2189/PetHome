3d5d1d74e1e4b3a46c401d9acfebedb99ca5b303
lgl2189 <1214525891@qq.com>
2025-05-02 15:12:28 +0800

    添加测试工具JwtSecretGenerator

---
+++ src/test/java/com/pethome/util/JwtSecretGenerator.java
@@ -0,0 +1,27 @@
+package com.pethome.util;
+
+import cn.hutool.jwt.JWTUtil;
+import cn.hutool.jwt.signers.JWTSignerUtil;
+import org.apache.tomcat.util.codec.binary.Base64;
+
+import java.security.SecureRandom;
+
+/**
+ * @author ：李冠良
+ * @description ：无描述
+ * @date ：2025 5月 02 14:43
+ */
+
+
+public class JwtSecretGenerator {
+    public static void main(String[] args) {
+        // 创建一个SecureRandom实例用于生成随机字节
+        SecureRandom secureRandom = new SecureRandom();
+        byte[] keyBytes = new byte[32];// 生成32字节的随机字节，对应256位
+        secureRandom.nextBytes(keyBytes);
+
+        // 使用Base64对生成的字节数组进行编码，得到一个可打印的字符串形式的密钥
+        String jwtSecret = Base64.encodeBase64String(keyBytes);
+        System.out.println("生成的JWT密钥: " + jwtSecret);
+    }
+}
