75a0a343f7426253cb10be443c16415eef0d28d0
lgl2189 <1214525891@qq.com>
2025-05-11 20:48:24 +0800

    修复SwaggerUi无法使用的问题

--- src/main/java/com/pethome/PetHomeApplication.java
+++ src/main/java/com/pethome/PetHomeApplication.java
@@ -1,10 +1,15 @@
 package com.pethome;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@OpenAPIDefinition
+@OpenAPIDefinition(
+        servers = {
+                @Server(url = "http://localhost:8080", description = "开发环境")
+        }
+)
 @SpringBootApplication
 public class PetHomeApplication {
 
