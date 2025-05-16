521e8abf31e0043a6cc3da7e0efa229d0efca8d0
lgl2189 <1214525891@qq.com>
2025-05-16 21:21:52 +0800

    添加一个函数结果包装器，便于返回更详细的函数执行结果

---
+++ src/main/java/com/pethome/entity/wrapper/ResultWrapper.java
@@ -0,0 +1,23 @@
+package com.pethome.entity.wrapper;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author ：Star
+ * @description ：无描述
+ * @date ：2025 5月 16 20:53
+ */
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResultWrapper {
+
+    private boolean succeeded;
+    private Object result;
+    private Object extraInfo;
+
+    public ResultWrapper(boolean succeeded, Object result) {}
+}
