9e729e05f92c9122a80dccb6df81bcee736f09fe
lgl2189 <1214525891@qq.com>
2025-05-25 18:45:22 +0800

    添加分页工具包，添加相关实体类和工具类

---
+++ src/main/java/com/pethome/entity/web/PageInfo.java
@@ -0,0 +1,37 @@
+package com.pethome.entity.web;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author ：Star
+ * @description ：包装分页信息的实体类，用于返回前端分页信息
+ * @date ：2025 5月 25 16:40
+ */
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PageInfo {
+    /**
+     * 当前页码
+     */
+    private int pageNumNow;
+    /**
+     * 总页数
+     */
+    private int pageNumTotal;
+    /**
+     * 当前页的记录数
+     */
+    private int pageSizeNow;
+    /**
+     * 每页的记录数
+     */
+    private int pageSize;
+    /**
+     * 总记录数，即数据库查询总条数
+     */
+    private long recordNum;
+}
---
+++ src/main/java/com/pethome/util/DatabasePageUtil.java
@@ -0,0 +1,27 @@
+package com.pethome.util;
+
+import com.pethome.entity.web.PageInfo;
+
+/**
+ * @author ：Star
+ * @description ：查询分页工具类
+ * @date ：2025 5月 25 16:38
+ */
+
+
+public class DatabasePageUtil {
+    public static <T> PageInfo getPageInfo(com.github.pagehelper.PageInfo<T> pageInfo) {
+        PageInfo pageInfoWrapper = new PageInfo();
+        // 当前页页数
+        pageInfoWrapper.setPageNumNow(pageInfo.getPageNum());
+        // 总页数
+        pageInfoWrapper.setPageNumTotal(pageInfo.getPages());
+        // 当前页记录数
+        pageInfoWrapper.setPageSizeNow(pageInfo.getSize());
+        // 每页记录数
+        pageInfoWrapper.setPageSize(pageInfo.getPageSize());
+        // 总记录数
+        pageInfoWrapper.setRecordNum(pageInfo.getTotal());
+        return pageInfoWrapper;
+    }
+}
