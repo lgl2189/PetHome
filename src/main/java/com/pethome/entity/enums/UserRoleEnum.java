047ec36421febc8b22135740cdc577691dde806a
lgl2189 <1214525891@qq.com>
2025-05-29 19:22:29 +0800

    从用户角色枚举类中删除超级管理员角色，因为普通用户不可能为超级管理员

--- src/main/java/com/pethome/entity/enums/UserRoleEnum.java
+++ src/main/java/com/pethome/entity/enums/UserRoleEnum.java
@@ -7,8 +7,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Getter
-public enum RoleEnum {
-    SUPER("super"),
+public enum UserRoleEnum {
     ADMIN("admin"),
     VOLUNTEER("volunteer"),
     ADOPTER("adopter"),
@@ -17,15 +16,15 @@ public enum RoleEnum {
 
     @EnumValue
     private final String value;
-    private static final Map<String, RoleEnum> VALUE_MAP = new HashMap<>();
+    private static final Map<String, UserRoleEnum> VALUE_MAP = new HashMap<>();
 
     static {
-        for (RoleEnum role : RoleEnum.values()) {
+        for (UserRoleEnum role : UserRoleEnum.values()) {
             VALUE_MAP.put(role.getValue(), role);
         }
     }
 
-    RoleEnum(String value) {
+    UserRoleEnum(String value) {
         this.value = value;
     }
 
@@ -34,7 +33,7 @@ public enum RoleEnum {
      * @param value 枚举值
      * @return 枚举对象
      */
-    public static RoleEnum fromValue(String value) {
+    public static UserRoleEnum fromValue(String value) {
         return value == null ? NORMAL : VALUE_MAP.getOrDefault(value, NORMAL);
     }
 }
