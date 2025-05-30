032d75426d86498a1360f11b41f9cdf458ec5c57
lgl2189 <1214525891@qq.com>
2025-05-30 18:47:48 +0800

    合并RescueStationController和RescueRecordController类，统一使用RescueController类

--- src/main/java/com/pethome/controller/RescueRecordController.java
+++ src/main/java/com/pethome/controller/RescueRecordController.java
@@ -1,7 +1,20 @@
 package com.pethome.controller;
 
+import com.pethome.dto.Result;
+import com.pethome.entity.mybatis.RescueStation;
+import com.pethome.service.RescueRecordService;
+import com.pethome.service.RescueStationService;
+import com.pethome.util.ResultUtil;
+import com.star.jwt.annotation.JwtAuthority;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.Assert;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * <p>
@@ -12,7 +25,26 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2025-04-27
  */
 @RestController
-@RequestMapping("/rescueRecord")
+@RequestMapping("/rescue")
 public class RescueRecordController {
+    private final RescueStationService rescueStationService;
+    private final RescueRecordService rescueRecordService;
+
+    @Autowired
+    public RescueRecordController(RescueStationService rescueStationService,RescueRecordService rescueRecordService) {
+        Assert.notNull(rescueStationService, "rescueStationService must not be null");
+        Assert.notNull(rescueRecordService, "rescueRecordService must not be null");
+        this.rescueStationService = rescueStationService;
+        this.rescueRecordService = rescueRecordService;
+    }
+
+    @JwtAuthority(enabled = false)
+    @GetMapping("/station/public/info/list")
+    public Result getRescueStationList(){
+        List<RescueStation> rescueStationList = rescueStationService.getPublicInfoList();
+        Map<String, Object> rescueStationMap = new HashMap<>();
+        rescueStationMap.put("station_info", rescueStationList);
+        return ResultUtil.success_200(rescueStationMap);
+    }
 
 }
