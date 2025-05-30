04968061ff9574f06c3746f9da1dd0fe848eae2b
lgl2189 <1214525891@qq.com>
2025-05-30 15:11:27 +0800

    添加获取全部权限列表接口

--- src/main/java/com/pethome/controller/RoleController.java
+++ src/main/java/com/pethome/controller/RoleController.java
@@ -1,7 +1,19 @@
 package com.pethome.controller;
 
+import com.pethome.dto.Result;
+import com.pethome.entity.mybatis.Role;
+import com.pethome.service.RoleService;
+import com.pethome.util.ResultUtil;
+import com.star.jwt.annotation.JwtAuthority;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.Assert;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * <p>
@@ -14,4 +26,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/role")
 public class RoleController {
+
+    private final RoleService roleService;
+
+    @Autowired
+    public RoleController(RoleService roleService) {
+        Assert.notNull(roleService, "roleService must not be null");
+        this.roleService = roleService;
+    }
+
+    @JwtAuthority
+    @GetMapping("/list")
+    public Result getRoleList(){
+        List<Role> roleList = roleService.list();
+        Map<String, Object> resMap = new HashMap<>();
+        resMap.put("role_list", roleList);
+        return ResultUtil.success_200(resMap,"获取角色列表成功");
+    }
+
 }
