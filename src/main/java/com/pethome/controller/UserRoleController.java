6617abf9a79bbce545c4519152f2768ca9fa7eeb
lgl2189 <1214525891@qq.com>
2025-05-30 14:14:43 +0800

    修正为@RestController

--- src/main/java/com/pethome/controller/UserRoleController.java
+++ src/main/java/com/pethome/controller/UserRoleController.java
@@ -1,7 +1,7 @@
 package com.pethome.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * <p>
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @author lgl
  * @since 2025-04-29
  */
-@Controller
+@RestController
 @RequestMapping("/userRole")
 public class UserRoleController {
 
